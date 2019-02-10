10ae1a53863ff7dc0fb91a35955c0ffc594f4ec8
CHARLENE GREEN <charlenecgreen@gmail.com>
2019-02-10 18:00:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function happyHolidayTo (holiday, name) {
   return "Happy Independence Day, you!"
 }
 function holidayCountdown (holiday, days) {
-  return 'It\'s 5 days until Independence Day!'
+  return 'It\'s 20 days until Mother\'s Day!'
 }
